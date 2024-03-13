c916e67357fad7ab3404e23de01768618ad73e96
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-03-13 21:05:16 +0500

    first commit

---
+++ Assignment 34/main.js
@@ -0,0 +1,5 @@
+var pizzas = ["pepperon", "margherits", "hawaiian"];
+pizzas.forEach(function (pizza) {
+    console.log("I like ".concat(pizza, " pizza."));
+});
+console.log("I really love pizza!");
---
+++ Assignment 34/main.ts
@@ -0,0 +1,6 @@
+let pizzas: string[] =["pepperon" , "margherits" , "hawaiian"];
+
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+});
+    console.log("I really love pizza!");
